539f4cd4aa9c254710ee23fe7109f63b5613a006
Gerald Schmittinger <geraschm18@computercamp.local>
2018-08-06 16:00:14 +0200

    Added GIVING_ITEM NPC type.

--- src/de/computercamp/rpg/entities/NPC.java
+++ src/de/computercamp/rpg/entities/NPC.java
@@ -3,11 +3,20 @@ package de.computercamp.rpg.entities;
 import de.computercamp.rpg.Vector2D;
 
 public class NPC extends BaseObject{
-	public enum NPCType {TALKING};
+	public enum NPCType {TALKING, GIVING_ITEM};
+	private NPCType type;
 	private String message;
+	private Item toGive;
 	public NPC(Vector2D position, String message) {
 		super(position);
 		this.message = message;
+		type = NPCType.TALKING;
+	}
+	public NPC(Vector2D position, String message, Item toGive) {
+		super(position);
+		this.message = message;
+		type = NPCType.GIVING_ITEM;
+		this.toGive = toGive;
 	}
 	@Override
 	public void onPlayerMove(Player player) {
@@ -15,6 +24,9 @@ public class NPC extends BaseObject{
 		if ((Math.abs(ppos.x-getPosition().x) == 1 && ppos.y == getPosition().y) || 
 				(Math.abs(ppos.y-getPosition().y) == 1 && ppos.x == getPosition().x)){
 			player.sendMessage(message);
+			if (type == NPCType.GIVING_ITEM) {
+				player.collectItem(toGive);
+			}
 		}
 	}
 	@Override
