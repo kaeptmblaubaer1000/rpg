9fce742724a023f96a787e18a6bb86d36d09c945
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-06 10:56:22 +0200

    Extend WallTile

--- src/de/computercamp/rpg/WallTile.java
+++ src/de/computercamp/rpg/WallTile.java
@@ -1,7 +1,21 @@
 package de.computercamp.rpg;
 
 public class WallTile extends BaseObject {
-    public WallTile(Vector2D position) {
+    public enum Type {
+        VERTICAL,
+        HORIZONTAL,
+
+        LEFT_UPPER_EDGE,
+        RIGHT_UPPER_EDGE,
+        LEFT_LOWER_EDGE,
+        RIGHT_LOWER_EDGE,
+
+    }
+
+    private Type type;
+
+    public WallTile(Vector2D position, Type type) {
         super(position);
+        this.type = type;
     }
 }
