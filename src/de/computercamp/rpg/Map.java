bc2f3f58487d0317acae4084a82ef1b75009fc97
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-06 10:18:25 +0200

    dd edge constants

--- src/de/computercamp/rpg/Map.java
+++ src/de/computercamp/rpg/Map.java
@@ -3,4 +3,9 @@ package de.computercamp.rpg;
 public class Map {
     public static final String VERTICAL_LINE = "|";
     public static final String HORIZONTAL_LINE = "-";
+
+    public static final String LEFT_UPPER_EDGE = "+";
+    public static final String RIGHT_UPPER_EDGE = "+";
+    public static final String LEFT_LOWER_EDGE = "+";
+    public static final String RIGHT_LOWER_EDGE = "+";
 }
