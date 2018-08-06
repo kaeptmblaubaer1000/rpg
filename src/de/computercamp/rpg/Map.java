7422f395b7bd3d84b108f9cf9e1605580725afec
Jan-Andrej Diehl <jan-dieh18@computercamp.local>
2018-08-06 15:03:58 +0200

    Unicode geaendert

--- src/de/computercamp/rpg/Map.java
+++ src/de/computercamp/rpg/Map.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Map {
-    public static final char VERTICAL_LINE = '│';
-    public static final char HORIZONTAL_LINE = '─';
+    public static final char VERTICAL_LINE = '─';
+    public static final char HORIZONTAL_LINE = '│';
 
     public static final char LEFT_UPPER_EDGE = '┌';
     public static final char RIGHT_UPPER_EDGE = '┐';
