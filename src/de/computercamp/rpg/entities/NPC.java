58fbbd870972c5c5451201abefb21d42dc6dd958
Lennart Kloock <lenni@skatnet.de>
2018-08-06 15:54:33 +0200

    Fixed NPC

--- src/de/computercamp/rpg/entities/NPC.java
+++ src/de/computercamp/rpg/entities/NPC.java
@@ -3,8 +3,9 @@ package de.computercamp.rpg.entities;
 import de.computercamp.rpg.Vector2D;
 
 public class NPC extends BaseObject{
-	public enum NPCType {TALKING};
-	private String message;
+    public enum NPCType {TALKING}
+
+    private String message;
 	public NPC(Vector2D position, String message) {
 		super(position);
 		this.message = message;
@@ -12,14 +13,13 @@ public class NPC extends BaseObject{
 	@Override
 	public void onPlayerMove(Player player) {
 		Vector2D ppos = player.getPosition();
-		if ((Math.abs(ppos.x-getPosition().x) == 1 && ppos.y == getPosition().y) || 
-				(Math.abs(ppos.y-getPosition().y) == 1 && ppos.x == getPosition().x)){
+        if ((Math.abs(ppos.x - position.x) == 1 && ppos.y == position.y) ||
+                (Math.abs(ppos.y - position.y) == 1 && ppos.x == position.x)) {
 			player.sendMessage(message);
 		}
 	}
 	@Override
 	public char render() {
-		// TODO Auto-generated method stub
 		return '\u2638';
 	}
 }
