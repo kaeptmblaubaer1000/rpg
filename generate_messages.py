8f8ff8a0280d3f00df7ff19520f1501ad708192b
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-07 14:59:27 +0200

    Improve Messages class, add changeLanguage

--- generate_messages.py
+++ generate_messages.py
@@ -6,18 +6,35 @@ with open("src/de/computercamp/rpg/resources/MessageBundle.properties", "rt", en
         names.append(name)
 
 with open("src/de/computercamp/rpg/resources/Messages.java", "wt", encoding="utf-8") as fp:
-    fragment = "\n".join(f"    public static String {name} = bundle.getString(\"{name}\");" for name in names)
+    fragment1 = "\n".join(f"    public static String {name};" for name in names)
+    fragment2 = "\n".join(f"        {name} = bundle.getString(\"{name}\");" for name in names)
     fp.write(f"""package de.computercamp.rpg.resources;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Messages {{
-    private static ResourceBundle bundle = ResourceBundle.getBundle("de.computercamp.rpg.resources.MessageBundle", Locale.getDefault());
+    private static ResourceBundle bundle;
+    private static Locale locale;
 
-{fragment}
+{fragment1}
 
     private Messages() {{
+    }}
+
+    private static void reloadStrings() {{
+        bundle = ResourceBundle.getBundle("de.computercamp.rpg.resources.MessageBundle", locale)
+{fragment2}
+    }}
+
+    static {{
+        locale = Locale.getDefault();
+        reloadStrings();
+    }}
+
+    public static void changeLanguage(Locale locale) {{
+        Messages.locale = locale;
+        reloadStrings();
     }}
 }}
 """)
--- src/de/computercamp/rpg/resources/Messages.java
+++ src/de/computercamp/rpg/resources/Messages.java
@@ -4,19 +4,44 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Messages {
-    private static ResourceBundle bundle = ResourceBundle.getBundle("de.computercamp.rpg.resources.MessageBundle", Locale.getDefault());
+    private static ResourceBundle bundle;
+    private static Locale locale;
 
-    public static String npcWelcome = bundle.getString("npcWelcome");
-    public static String npcMagician = bundle.getString("npcMagician");
-    public static String npcBadMagician = bundle.getString("npcBadMagician");
-    public static String npcWeaponsmith = bundle.getString("npcWeaponsmith");
-    public static String closeProgram = bundle.getString("closeProgram");
-    public static String healing_potion = bundle.getString("healing_potion");
-    public static String inventory = bundle.getString("inventory");
-    public static String key = bundle.getString("key");
-    public static String sword = bundle.getString("sword");
-    public static String npcWaiting = bundle.getString("npcWaiting");
+    public static String npcWelcome;
+    public static String npcMagician;
+    public static String npcBadMagician;
+    public static String npcWeaponsmith;
+    public static String closeProgram;
+    public static String healing_potion;
+    public static String inventory;
+    public static String key;
+    public static String sword;
+    public static String npcWaiting;
 
     private Messages() {
+    }
+
+    private static void reloadStrings() {
+        bundle = ResourceBundle.getBundle("de.computercamp.rpg.resources.MessageBundle", locale)
+        npcWelcome = bundle.getString("npcWelcome");
+        npcMagician = bundle.getString("npcMagician");
+        npcBadMagician = bundle.getString("npcBadMagician");
+        npcWeaponsmith = bundle.getString("npcWeaponsmith");
+        closeProgram = bundle.getString("closeProgram");
+        healing_potion = bundle.getString("healing_potion");
+        inventory = bundle.getString("inventory");
+        key = bundle.getString("key");
+        sword = bundle.getString("sword");
+        npcWaiting = bundle.getString("npcWaiting");
+    }
+
+    static {
+        locale = Locale.getDefault();
+        reloadStrings();
+    }
+
+    public static void changeLanguage(Locale locale) {
+        Messages.locale = locale;
+        reloadStrings();
     }
 }
