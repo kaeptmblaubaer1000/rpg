70e55d4dc6bc2f00bd9d627f7c6ec9e1bc974ca2
Lennart Kloock <lenni@skatnet.de>
2018-08-07 10:22:05 +0200

    Fixed Main (encoding)

--- src/de/computercamp/rpg/Main.java
+++ src/de/computercamp/rpg/Main.java
@@ -1,23 +1,11 @@
 package de.computercamp.rpg;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.EventQueue;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Toolkit;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.text.DefaultCaret;
 
 public class Main {
 	private static JButton closeButton;
@@ -41,8 +29,8 @@ public class Main {
 		ta.addKeyListener(new KeyHandler());
 		ta.setBackground(Color.black);
 		ta.setForeground(Color.white);
-		ta.setEditable(false); 
-		closeButton = new JButton("Schlieﬂen");
+		ta.setEditable(false);
+        closeButton = new JButton("Schlie\u00DFn");
 		closeButton.setBackground(Color.red);
 		closeButton.setForeground(Color.white);
 		closeButton.addActionListener(new CloseHandler());
