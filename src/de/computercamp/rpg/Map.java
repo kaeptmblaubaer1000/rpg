7c71d09e085d515ee573d27298ed1f695e10eb15
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-07 10:09:24 +0200

    Add no-op Map.render

--- src/de/computercamp/rpg/Map.java
+++ src/de/computercamp/rpg/Map.java
@@ -20,14 +20,14 @@ public class Map {
     private List<BaseObject> mapContents = new ArrayList<>();
 
     public boolean removeObject(BaseObject base) {
-        if(base.getMap() == this)
+        if (base.getMap() == this)
             base.setMap(null);
         return mapContents.removeIf((object) -> base == object);
     }
 
     public void addObject(BaseObject object) {
         object.setMap(this);
-        if(!mapContents.contains(object)) {
+        if (!mapContents.contains(object)) {
             mapContents.add(object);
         }
     }
@@ -37,11 +37,36 @@ public class Map {
     }
 
     public boolean onPlayerMove(Player player) {
-        for(BaseObject object: mapContents) {
+        for (BaseObject object : mapContents) {
             if (!object.onPlayerMove(player)) {
                 return false;
             }
         }
         return true;
+    }
+
+    public String render() {
+        int maxX = mapContents.stream().mapToInt((object) -> object.getPosition().x).max().orElse(-1) + 1;
+        int maxY = mapContents.stream().mapToInt((object) -> object.getPosition().y).max().orElse(-1) + 1;
+
+        StringBuilder whole = new StringBuilder();
+        List<StringBuilder> strings = new ArrayList<>();
+
+        for (int y = 0; y < maxY; y++) {
+            StringBuilder row = new StringBuilder();
+            for (int x = 0; x < maxX; x++) {
+                row.append(" ");
+            }
+
+            strings.add(row);
+        }
+
+
+        for (StringBuilder builder : strings) {
+            whole.append(builder);
+            whole.append('\n');
+        }
+
+        return whole.toString();
     }
 }
