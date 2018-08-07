528508013099fe5ff60b98a60e5488a98c7974ae
Gerald Schmittinger <geraschm18@computercamp.local>
2018-08-07 15:04:59 +0200

    Added support for new Messages class in NPCSpawner class

--- src/de/computercamp/rpg/NPCSpawner.java
+++ src/de/computercamp/rpg/NPCSpawner.java
@@ -2,13 +2,14 @@ package de.computercamp.rpg;
 
 import de.computercamp.rpg.entities.Item;
 import de.computercamp.rpg.entities.NPC;
+import de.computercamp.rpg.resources.Messages;
 
 public class NPCSpawner {
 	public void spawnRandomNPCs(Map map, Vector2D minPos, Vector2D maxPos) {
-		NPC welcomeNPC = new NPC(getRandomLocation(minPos, maxPos), Main.GetLanguageText("npcWelcome"));
-		NPC magician = new NPC(getRandomLocation(minPos, maxPos), Main.GetLanguageText("npcMagician"), new Item(getRandomLocation(minPos, maxPos), Item.Type.HEALING_POTION), 999999999);
-		NPC badmagician = new NPC(getRandomLocation(minPos, maxPos), Main.GetLanguageText("npcBadMagician"), -10, 10, 30000);
-		NPC weaponsmith = new NPC(getRandomLocation(minPos, maxPos), Main.GetLanguageText("npcWeaponsmith"), new Item(getRandomLocation(minPos, maxPos), Item.Type.SWORD), 999999999);
+		NPC welcomeNPC = new NPC(getRandomLocation(minPos, maxPos), Messages.npcWelcome);
+		NPC magician = new NPC(getRandomLocation(minPos, maxPos), Messages.npcMagician, new Item(getRandomLocation(minPos, maxPos), Item.Type.HEALING_POTION), 999999999);
+		NPC badmagician = new NPC(getRandomLocation(minPos, maxPos), Messages.npcBadMagician, -10, 10, 30000);
+		NPC weaponsmith = new NPC(getRandomLocation(minPos, maxPos), Messages.npcWeaponsmith, new Item(getRandomLocation(minPos, maxPos), Item.Type.SWORD), 999999999);
 		map.addObject(welcomeNPC);
 		map.addObject(magician);
 		map.addObject(badmagician);
