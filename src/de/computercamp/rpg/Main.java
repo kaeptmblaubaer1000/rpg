3c1d97b88ea3dd7199e9787da79922b62626a86b
Jan-Andrej Diehl <jan-dieh18@computercamp.local>
2018-08-08 14:59:45 +0200

    Add a select language dialog on start

--- src/de/computercamp/rpg/Main.java
+++ src/de/computercamp/rpg/Main.java
@@ -22,7 +22,21 @@ public class Main {
     private static MapBuilder mapBuilder = new MapBuilder();
 
     public static void main(String[] args) {
-        jf = new JFrame("");
+    	if(JOptionPane.showOptionDialog(null, "Which language do you want?","Select language",
+    	                JOptionPane.YES_NO_CANCEL_OPTION,
+    	                JOptionPane.WARNING_MESSAGE, null, 
+    	                new String[]{"English", "German"}, "English") == 0) {
+    		Messages.locale = Locale.GERMAN;
+    	}
+    	else {
+    		Messages.locale = Locale.ENGLISH;
+    	}
+    	createJFrame();
+        
+    }
+    
+    private static void createJFrame() {
+    	jf = new JFrame("");
         ta = new JTextArea(Toolkit.getDefaultToolkit().getScreenSize().width,
                 Toolkit.getDefaultToolkit().getScreenSize().height - 5);
         try {
@@ -43,14 +57,6 @@ public class Main {
         ta.setFocusable(true);
         JPanel panel = new JPanel();
         Locale[] languageList = {Locale.GERMAN, Locale.ENGLISH};
-        JComboBox<Locale> selectLanguageComboBox = new JComboBox<Locale>(languageList);
-        if (Locale.getDefault().equals(Locale.GERMAN))
-            selectLanguageComboBox.setSelectedIndex(0);
-        else
-            selectLanguageComboBox.setSelectedIndex(1);
-        selectLanguageComboBox.addActionListener(new SelectLanguageHandler());
-        selectLanguageComboBox.setBackground(Color.green);
-        selectLanguageComboBox.setForeground(Color.black);
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setOpaque(true);
         panel.add(ta);
