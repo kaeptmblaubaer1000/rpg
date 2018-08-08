ab055a9c4bfef95df2e01b567f1851df5a431dd2
Lennart Kloock <lenni@skatnet.de>
2018-08-08 09:39:27 +0200

    Reformat Item class

--- src/de/computercamp/rpg/entities/Item.java
+++ src/de/computercamp/rpg/entities/Item.java
@@ -3,47 +3,46 @@ package de.computercamp.rpg.entities;
 
 import de.computercamp.rpg.Vector2D;
 
-public class Item extends BaseObject{
+public class Item extends BaseObject {
 
-	public enum Type {HEALING_POTION, SWORD, KEY}
+    private Type type;
+    private char symbol;
+    private String displayName;
 
-	private Type type;
-	private char symbol;
-	private String displayName;
-	public Item(Vector2D pos, Type type) {
-		super(pos);
-		this.type = type;
-		if (type == Type.HEALING_POTION) {
-			symbol = '\u1e50';
-			displayName = "Healing potion";
-		} else if (type == Type.SWORD) {
-			symbol = '\u1e36';
-			displayName = "Sword";
-		} else if (type == Type.KEY) {
-			symbol = '\ua720';
-			displayName = "Key";
-		}else {
-			symbol = 'E';
-			displayName = "!UNKNOWN!";
-		}
-	}
-	
-	
-	public Type getType() {
-		return type;
-	}
-	public char getSymbol() {
-		return symbol;
-	}
-	public String getDisplayName() {
-		return displayName;
-	}
+    public Item(Vector2D pos, Type type) {
+        super(pos);
+        this.type = type;
+        if (type == Type.HEALING_POTION) {
+            symbol = '\u1e50';
+            displayName = "Healing potion";
+        } else if (type == Type.SWORD) {
+            symbol = '\u1e36';
+            displayName = "Sword";
+        } else if (type == Type.KEY) {
+            symbol = '\ua720';
+            displayName = "Key";
+        } else {
+            symbol = 'E';
+            displayName = "!UNKNOWN!";
+        }
+    }
 
+    public Type getType() {
+        return type;
+    }
 
-	@Override
-	public char render() {
-		return symbol;
-	}
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public char render() {
+        return symbol;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -59,4 +58,6 @@ public class Item extends BaseObject{
     public int hashCode() {
         return type != null ? type.hashCode() : 0;
     }
+
+    public enum Type {HEALING_POTION, SWORD, KEY}
 }
