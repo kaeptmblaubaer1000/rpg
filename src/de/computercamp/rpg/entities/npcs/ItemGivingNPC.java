3f31e71ce341ee319ff0c90866c6ec854bacacda
Jan-Andrej Diehl <jan-dieh18@computercamp.local>
2018-08-09 09:49:57 +0200

    Set the inventory on the right site and change the size of the textarea on the right site

--- src/de/computercamp/rpg/entities/npcs/ItemGivingNPC.java
+++ src/de/computercamp/rpg/entities/npcs/ItemGivingNPC.java
@@ -1,11 +1,9 @@
 package de.computercamp.rpg.entities.npcs;
 
-import java.text.MessageFormat;
 
 import de.computercamp.rpg.Vector2D;
 import de.computercamp.rpg.entities.Player;
 import de.computercamp.rpg.entities.items.Item;
-import de.computercamp.rpg.resources.Messages;
 
 public class ItemGivingNPC extends NPC{
 	private Item toGive;
