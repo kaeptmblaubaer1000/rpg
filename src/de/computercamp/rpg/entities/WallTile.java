f9ab03df81610acc22edefe90de5ed2a1403ab11
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-10 14:07:28 +0200

    Add missing T connector

--- src/de/computercamp/rpg/entities/WallTile.java
+++ src/de/computercamp/rpg/entities/WallTile.java
@@ -16,6 +16,7 @@ public class WallTile extends BaseObject {
         VERTICAL_RIGHT_T_CONNECTOR,
         VERTICAL_LEFT_T_CONNECTOR,
         HORIZONTAL_DOWN_T_CONNECTOR,
+        HORIZONTAL_UP_T_CONNECTOR,
     }
 
     private Type type;
@@ -45,6 +46,8 @@ public class WallTile extends BaseObject {
                 return Map.VERTICAL_LEFT_T_CONNECTOR;
             case HORIZONTAL_DOWN_T_CONNECTOR:
                 return Map.HORIZONTAL_DOWN_T_CONNECTOR;
+            case HORIZONTAL_UP_T_CONNECTOR:
+                return Map.HORIZONTAL_UP_T_CONNECTOR;
             default:
                 return 'E';
         }
