f1ea4a3383b68ac26f661c529d20e50065aac6d5
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-10 15:34:42 +0200

    Render NPCs on top

--- src/de/computercamp/rpg/Map.java
+++ src/de/computercamp/rpg/Map.java
@@ -2,6 +2,7 @@ package de.computercamp.rpg;
 
 import de.computercamp.rpg.entities.BaseObject;
 import de.computercamp.rpg.entities.Player;
+import de.computercamp.rpg.entities.npcs.NPC;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -81,6 +82,10 @@ public class Map {
         }
 
         for (BaseObject object : mapContents.stream().filter((object) -> object instanceof Player).collect(Collectors.toList())) {
+            strings.get(object.getPosition().y).setCharAt(object.getPosition().x, object.render());
+        }
+
+        for (BaseObject object : mapContents.stream().filter((object) -> object instanceof NPC).collect(Collectors.toList())) {
             strings.get(object.getPosition().y).setCharAt(object.getPosition().x, object.render());
         }
 
