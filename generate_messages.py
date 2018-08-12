e35c34961ce19a232e33061f558e17cfb2c66df1
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-12 13:52:21 +0200

    Update Python script for new source directories

--- generate_messages.py
+++ generate_messages.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-with open("src/de/computercamp/rpg/resources/MessageBundle.properties", "rt", encoding="utf-8") as fp:
+with open("src/main/resources/de/computercamp/rpg/resources/MessageBundle.properties", "rt", encoding="utf-8") as fp:
     names = []
     for line in fp:
         name = line.partition("=")[0]
         names.append(name)
 
-with open("src/de/computercamp/rpg/resources/Messages.java", "wt", encoding="utf-8") as fp:
+with open("src/main/java/de/computercamp/rpg/resources/Messages.java", "wt", encoding="utf-8") as fp:
     fragment1 = "\n".join(f"    public static String {name};" for name in names)
     fragment2 = "\n".join(f"        {name} = bundle.getString(\"{name}\");" for name in names)
     fp.write(f"""package de.computercamp.rpg.resources;
