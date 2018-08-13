36b7368756972d77bac0faa6e14c883ee29fc25d
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-13 15:09:49 +0200

    Implement Writer.writeBigInt

--- src/main/java/de/computercamp/rpg/serialization/Writer.kt
+++ src/main/java/de/computercamp/rpg/serialization/Writer.kt
@@ -3,6 +3,13 @@ package de.computercamp.rpg.serialization
 class Writer(private val sink: ByteSink) : ByteSink {
     override fun writeByte(value: Short) = sink.writeByte(value)
 
-    fun writeInt(value: Int) {
+    fun writeBigInt(value: Int) {
+        var tempValue = value
+        val bytes = mutableListOf<Short>()
+
+        while (tempValue > 0) {
+            bytes.add(0, (value and 255).toShort())
+            tempValue = tempValue ushr 8
+        }
     }
 }
