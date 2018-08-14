69158b5bd8d613873c40e0ff11098b062cb1af14
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-14 13:13:20 +0200

    Make it possible to customize how much a Coin is worth

--- src/main/java/de/computercamp/rpg/entities/items/Coin.java
+++ src/main/java/de/computercamp/rpg/entities/items/Coin.java
@@ -5,15 +5,21 @@ import de.computercamp.rpg.entities.Player;
 import de.computercamp.rpg.resources.Messages;
 
 public class Coin extends Item {
+    private final int worth;
+
+    public Coin(Vector2D position, int worth) {
+        super(position, false);
+        this.worth = worth;
+        symbol = '\u00A9';
+    }
 
     public Coin(Vector2D position) {
-        super(position, false);
-        symbol = '\u00A9';
+        this(position, 1);
     }
 
     @Override
     public void onCollect(Player player) {
-        player.addCoins(1);
+        player.addCoins(worth);
         map.removeObject(this);
     }
 
