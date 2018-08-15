a1da97902d075b216b676fff9ba16d8f71e07c1b
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-15 11:30:12 +0200

    Add a ByteSource method to read multiple bytes at a time

--- src/main/java/de/computercamp/rpg/serialization/ByteSource.kt
+++ src/main/java/de/computercamp/rpg/serialization/ByteSource.kt
@@ -1,8 +1,25 @@
 package de.computercamp.rpg.serialization
 
+import java.util.*
+
 interface ByteSource {
     /** Reads a byte from this source.
      * This function returns a [Short] because it's the smallest integer type that supports 255.
+     *
+     * If this returns -1, this source is used up.
      */
     fun readByte(): Short
+
+    fun readBytes(amoumt: Int): ShortArray {
+        val array = ShortArray(amoumt)
+        for(i in 0..amoumt) {
+            val byte = readByte()
+            if(byte == (-1).toShort())
+            {
+                return Arrays.copyOf(array, i)
+            }
+            array[i] = byte
+        }
+        return array
+    }
 }
