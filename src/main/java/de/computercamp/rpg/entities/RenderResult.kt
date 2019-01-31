39177f6d15952069d2e16060ae886142e2e3aff0
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2019-01-31 17:51:40 +0100

    Create RenderResult class

---
+++ src/main/java/de/computercamp/rpg/entities/RenderResult.kt
@@ -0,0 +1,10 @@
+package de.computercamp.rpg.entities
+
+import de.computercamp.rpg.block
+
+/**
+ * WARNING: Do not use multiple characters!
+ * This is only for UTF-16 surrogates!
+ */
+data class RenderResult(val char1: Char, val char2: Char) {
+}
