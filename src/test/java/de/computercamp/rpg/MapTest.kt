3ce870efa5b609cb9332a4be7b20502a3afb6fb8
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2019-02-04 10:24:42 +0100

    Implement (and @Ignore) first test

--- src/test/java/de/computercamp/rpg/MapTest.kt
+++ src/test/java/de/computercamp/rpg/MapTest.kt
@@ -2,8 +2,23 @@ package de.computercamp.rpg
 
 import kotlin.test.*
 
+@Ignore("Breaks randomly due to unpredictable dynamic map extensions")
 class MapTest {
+    var game: Game = Game.create()
+    var map: Map = game.currentRoom
+
+    @BeforeTest
+    fun setUp() {
+        game = Game.create()
+        map = game.currentRoom
+        // Clear the map here, because this isn't the test for MapBuilder
+        for (baseObject in map.mapContents.toTypedArray()) {
+            map.removeObject(baseObject)
+        }
+    }
+
     @Test
-    fun renderTest() {
+    fun testRenderNothing() {
+        assertEquals("${" ".repeat(60)}\n".repeat(16).dropLast(1), map.render());
     }
 }
